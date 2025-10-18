8b30a72cd94639f4ca0dd9baa45da5e7de5ab993
kayezsoftware <kartaca00@hotmail.com>
2025-10-19 00:06:44 +0300

    Completed Bedroom Composition Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wall wall1 = new Wall("North");
+        Wall wall2 = new Wall("South");
+        Wall wall3 = new Wall("East");
+        Wall wall4 = new Wall("West");
+
+        Ceiling ceiling = new Ceiling(3, PaintColor.WHITE);
+        Bed bed = new Bed("Modern", 2, 1, 2, 1);
+        Lamp lamp = new Lamp(LampType.NEON, true, 5);
+        Wardrobe wardrobe = new Wardrobe(2, 3, 60.5);
+        Carpet carpet = new Carpet(4, 3, PaintColor.RED);
+
+        Bedroom bedroom = new Bedroom("Yalçın’s Bedroom", wall1, wall2, wall3, wall4, ceiling, bed, lamp, wardrobe, carpet);
+
+        bedroom.getLamp().turnOn();
+        bedroom.getBed().make();
+        bedroom.getCeiling().create();
+        bedroom.getWall1().create();
+        bedroom.getWardrobe().add();
+        bedroom.getCarpet().lying();
     }
 }
